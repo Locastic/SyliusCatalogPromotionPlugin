ec925aba866fd617fb258b52dac287c395a40b9a
Jurica Šeparović <jsepar00@fesb.hr>
2018-06-18 18:03:22 +0200

    Initial commit

---
+++ src/SyliusCatalogPromotionPlugin.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Locastic\SyliusCatalogPromotionPlugin;
+
+use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class SyliusCatalogPromotionPlugin extends Bundle
+{
+    use SyliusPluginTrait;
+}
